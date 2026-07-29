2a0977e376ec6f602464841561510161963f74c3
Duha <duhaanter@gmail.com>
2026-07-30 01:54:40 +0300

    setup is done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="min-h-screen bg-primary flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold">NutriGuard is working</h1>
+      </div>
+    </>
+  );
+}
