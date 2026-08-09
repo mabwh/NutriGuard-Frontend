7b0b9b754d4d351296c292684df701cde822ee72
Duha <duhaanter@gmail.com>
2026-08-09 09:51:47 +0300

    Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,25 @@ import MainLayout from "./layouts/Mainlayout";
 import Dashboard from "./pages/Dashboard";
 import CreateHealthProfile from "./pages/CreateHealthProfile";
 import LoggedInlayout from "./layouts/LoggedInlayout";
-import Profile from './profile/screens/Profile'
+import Profile from "./profile/screens/Profile";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 export default function App() {
   return (
     <>
       <Routes>
+        
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-
-          <Route
-            path="/create-health-profile"
-            element={<CreateHealthProfile />}
-          />
         </Route>
-        <Route element={<LoggedInlayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/profile" element={<Profile/>} />
+
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/create-health-profile" element={<CreateHealthProfile />}/>
+          <Route element={<LoggedInlayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Route>
 
         <Route path="/login" element={<Login />} />
---
+++ src/utils/ProtectedRoutes.jsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { authStore } from "../store/auth";
+
+export default function ProtectedRoutes() {
+  const accessToken = authStore((state) => {
+    state.accessToken;
+  });
+  return accessToken ? <Outlet /> : <Navigate to="/login" />;
+}
